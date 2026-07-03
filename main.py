25460440fd524087cfcf1855b7ee29d07c840050
greenyarik0505-jpg <greenyarik0505-jpg@users.noreply.github.com>
2026-07-03 15:03:41 +0300

    Simplify main.py: remove functions and make it flat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 
-def main():
-    root = tk.Tk()
-    root.title("Окно")
-    root.geometry("400x300")
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.title("Окно")
+root.geometry("400x300")
+root.mainloop()
