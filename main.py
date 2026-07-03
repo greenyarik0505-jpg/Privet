1b13ba14711f011b7d06a58297955a115cdd448e
greenyarik0505-jpg <greenyarik0505-jpg@users.noreply.github.com>
2026-07-03 15:17:47 +0300

    Update main.py
    добавив лабел

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Окно")
 root.geometry("400x300")
+
+# Створення лабела (мітки)
+label = tk.Label(root, text="Привіт! Це мій текст", font=("Arial", 14))
+# Розміщення лабела у вікні
+label.pack(pady=50)
+
 root.mainloop()
